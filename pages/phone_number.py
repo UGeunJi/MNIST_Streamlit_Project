19ee12f7ae74ef57a71857ed3f7aefa7fad824e7
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-01-30 14:34:48 +0900

    Update phone_number.py

--- pages/phone_number.py
+++ pages/phone_number.py
@@ -16,7 +16,7 @@ model = load()
 
 st.image('./images/title.png')
 
-name_input = st.text_input('이름을 입력하세요')
+name_input = st.text_input('이름(Only English)을 입력하세요')
 
 st.subheader(" '010'을 제외한 휴대폰 번호 8자리를 입력해주세요.")
 
